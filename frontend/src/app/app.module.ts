6aebfd57baca53f027bc9f72effdde0eed3eca32
Patrick <paddy38@web.de>
2020-04-22 23:38:44 +0200

    added component for zutaten

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ZutatenListeComponent } from './zutaten-liste/zutaten-liste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZutatenListeComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/zutaten-liste/zutaten-liste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zutaten-liste',
+  templateUrl: './zutaten-liste.component.html',
+  styleUrls: ['./zutaten-liste.component.css']
+})
+export class ZutatenListeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
