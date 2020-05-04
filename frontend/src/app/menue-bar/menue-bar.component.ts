7ecc6f38cfb1975926a0374703cb0744475dfe24
Patrick <paddy38@web.de>
2020-05-04 14:47:58 +0200

    added go back button

--- frontend/src/app/menue-bar/menue-bar.component.ts
+++ frontend/src/app/menue-bar/menue-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-menue-bar',
@@ -10,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
  */
 export class MenueBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
   }
 
+  goBack() {
+    this.location.back();
+  }
 }
