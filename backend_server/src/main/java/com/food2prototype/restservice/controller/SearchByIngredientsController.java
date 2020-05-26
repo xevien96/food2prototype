99351336c131088e2b14b4c5172ac4b5c2d54421
Patrick <paddy38@web.de>
2020-05-26 09:58:51 +0200

    Anpassung des REST-Controllers zur Verwendung des Providers

--- backend_server/src/main/java/com/food2prototype/restservice/controller/SearchByIngredientsController.java
+++ backend_server/src/main/java/com/food2prototype/restservice/controller/SearchByIngredientsController.java
@@ -1,6 +1,7 @@
 package com.food2prototype.restservice.controller;
 
 import com.food2prototype.restservice.model.MockDB;
+import com.food2prototype.restservice.model.RecipeProvider;
 import com.food2prototype.restservice.model.Rezept;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +17,8 @@ public class SearchByIngredientsController {
 
     @GetMapping("/recipe/search")
     public String[] searchByIngredients(@RequestParam(value = "ingredients") String[] values) {
-        List<Rezept> recipes = MockDB.getAllRecipes();
-        List<String> recipeName = recipes.stream().map(rezept -> rezept.getName()).collect(Collectors.toList());
+        List<Rezept> recipes = RecipeProvider.getRecipesForIngredients(Arrays.asList(values));
+        List<String> recipeName = recipes.stream().map(Rezept::getName).collect(Collectors.toList());
         String[] ret = new String[recipes.size()];
         return recipeName.toArray(ret);
     }
