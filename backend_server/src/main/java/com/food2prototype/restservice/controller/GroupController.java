878dc5904a85c545e174ba2e7f3eaa90a43e825d
Patrick <paddy38@web.de>
2020-06-22 18:24:02 +0200

    Refactoring des GroupControllers

--- backend_server/src/main/java/com/food2prototype/restservice/controller/GroupController.java
+++ backend_server/src/main/java/com/food2prototype/restservice/controller/GroupController.java
@@ -3,7 +3,6 @@ package com.food2prototype.restservice.controller;
 import com.food2prototype.restservice.model.Group;
 import com.food2prototype.restservice.model.Ingredient;
 import com.food2prototype.restservice.model.Recipe;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -15,30 +14,28 @@ public class GroupController {
 
   @PostMapping("/group/{groupID}")
   public int addUser(@PathVariable(value = "groupID") int ID, @RequestBody String[] userIngredientsNames) {
-    Recipe recipe = Recipe.get(ID);
-    Set<Ingredient> userIngredients = new HashSet<>();
-    for (String ingString : userIngredientsNames) {
-      userIngredients.add(Ingredient.getIngredient(ingString));
-    }
     Group group = Group.get(ID);
-    group.addUserToGroup("user" + Math.random(), userIngredients);
-    return group.ID;
+    return addUserToGroup("", userIngredientsNames, group);
   }
 
   @PostMapping("/group/recipe/{recipeID}")
   public int createGroup(@PathVariable(value = "recipeID") int ID, @RequestBody String[] userIngredientsNames){
     Recipe recipe = Recipe.get(ID);
     Group newGroup = new Group(recipe);
-    Set<Ingredient> userIngredients = new HashSet<>();
-    for (String ingString : userIngredientsNames) {
-      userIngredients.add(Ingredient.getIngredient(ingString));
-    }
-    newGroup.addUserToGroup("user" + Math.random(), userIngredients);
-    return newGroup.ID;
+    return addUserToGroup("", userIngredientsNames, newGroup);
   }
 
   @GetMapping("/group/{groupID}")
   public Group getGroup(@PathVariable(value = "groupID") int ID) {
     return Group.get(ID);
+  }
+
+  private int addUserToGroup(String userName, String[] userIngredientsNames, Group group) {
+    Set<Ingredient> userIngredients = new HashSet<>();
+    for (String ingString : userIngredientsNames) {
+      userIngredients.add(Ingredient.getIngredient(ingString));
+    }
+    group.addUserToGroup(userName, userIngredients);
+    return group.ID;
   }
 }
